5773ce2b00ba56990948545be94aff2b91b87865
humairn <160233023+humairn@users.noreply.github.com>
2024-04-08 16:49:09 +0100

    testing

--- tests/fms_test.py
+++ tests/fms_test.py
@@ -28,7 +28,7 @@ class TestE2E(unittest.TestCase):
               az2                              = "eu-west-2b"
               naming_suffix                    = "apps-preprod-dq"
               environment                      = "prod"
-              rds_enhanced_monitoring_role     = "arn:aws:iam::123456789:role/rds-enhanced-monitoring-role"
+            
 
             }
         """
